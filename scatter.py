62dd0ea2d4138d824cb03e24edb82f0389b8059e
Dony franklin <83357822+Donyfranklin@users.noreply.github.com>
2025-01-27 09:07:38 +0530

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+names=['A','B','C','D','E']
+
+marks=[98,40,85,98,87]
+
+plt.plot(names,marks)
+
+plt.show()
+
+plt.scatter(names,marks)
+
+plt.show()
+
+
+plt.bar(names,marks)
+
+plt.show()
